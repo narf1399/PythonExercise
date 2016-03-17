aaec291c1c2827ea2ed59e1e3c089b9665eeefe8
Tim <tim.metzler@smail.inf.h-brs.de>
2016-03-17 13:11:53 +0100

    Some sympy examples

--- Python Algebra/sympyExamples.py
+++ Python Algebra/sympyExamples.py
@@ -2,6 +2,50 @@
 """
 Created on Thu Mar 17 12:42:24 2016
 
-@author: narf
+Some of this only seems to work in the console
+
+@author: Tim Metzler
 """
+
+from sympy import *
+# from sympy import symbols
+
+# Define variables as symbols (so we can change the value of those later)
+x = symbols('x')
+y = symbols('y')
+
+# Differentation
+print diff(cos(x), x)
+
+# Integration
+
+print integrate(ln(x), x)
+expr = integrate(exp(exp(x)),x)
+init_printing()
+print expr
+
+# Solve equations
+print simplify(Eq(x+1, 4))
+
+# Simple equations
+myEquation = Eq(x + 1, 4)
+print myEquation
+
+# Differential equations
+f, g = symbols('f g', cls=Function)
+diffeq = Eq(f(x).diff(x, x) - 2*f(x).diff(x) + f(x), sin(x))
+
+print diffeq
+
+# Solve
+print dsolve(diffeq, f(x))
+
+
+
+
+
+
+
+
+
 
